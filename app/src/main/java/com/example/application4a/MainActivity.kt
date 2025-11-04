86ff10ad53a5f1086e2800602099be09ec6f992f
utm-ncpa <nelson.padilla@ut-morelia.edu.mx>
2025-11-04 10:14:41 -0600

    Commit Inicial

---
+++ app/src/main/java/com/example/application4a/MainActivity.kt
@@ -0,0 +1,110 @@
+package com.example.application4a
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.application4a.ui.theme.Application4ATheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Application4ATheme{
+                Surface{
+                    // MessageCard(msg = Message("Lexi", "Hey, take a look at Jetpack Compose, it´s great!"))
+                    Conversation(SampleData.conversationSample)
+                }
+            }
+
+        }
+    }
+}
+
+@Composable
+fun MessageCard(name: String) {
+    Text(text = "Hello $name!")
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message){
+    Row(modifier = Modifier.padding(8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.profile_picture),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(60.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+            Column {
+            Text(text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall)
+            Spacer(modifier = Modifier.height(4.dp))
+            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp){
+                Text(text = msg.body,
+                    style = MaterialTheme.typography.bodyMedium)
+            }
+
+        }
+    }
+}
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground =  true,
+    name = "Dark Mode"
+)
+@Composable
+fun PreviewMessageCard() {
+    Application4ATheme {
+        Surface {
+            MessageCard(msg = Message("Lexi", "Hey, take a look at Jetpack Compose, it´s great!"))
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation(){
+    Application4ATheme {
+        Conversation(SampleData.conversationSample)
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>){
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
